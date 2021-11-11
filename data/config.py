cf682db6102e1e84862401338642c3c94fc4dcee
TimurPythonDev <karabayevtimurpython@gmail.com>
2021-11-11 16:06:16 +0300

    dfdffggfoken

--- data/config.py
+++ data/config.py
@@ -1,18 +1,20 @@
-# from environs import Env
-#
-# # # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
-#
-# # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
-# CHANELLS = ['@timurPythonDev',"@BlackCodersTeamOfficial",'@palitexnikainsitut']
+from environs import Env
 
-import os
+# # environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
 
-BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-ADMINS = str(os.environ.get("ADMINS"))
-IP = str(os.environ.get('ip'))
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("ip")  # Xosting ip manzili
 CHANELLS = ['@timurPythonDev',"@BlackCodersTeamOfficial",'@palitexnikainsitut']
+
+
+
+# import os
+#
+# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+# ADMINS = str(os.environ.get("ADMINS"))
+# IP = str(os.environ.get('ip'))
+# CHANELLS = ['@timurPythonDev',"@BlackCodersTeamOfficial",'@palitexnikainsitut']
